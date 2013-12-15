322a04732469f187154f49992b17f82572c88656
Travis Dempsey <dempsey.travis@gmail.com>
2013-12-15 16:30:25 -0600

    migrating laptops; push all loose code to git

---
+++ cards/zombies.rb
@@ -0,0 +1,75 @@
+ZOMBIES = 
+  [
+   { 
+     'name'  => 'evasive',
+     'power' => 1,
+     'text'  => 'Prevent all damage that would be done to this zombie.'
+   },
+   {
+     'name'  => 'spiky',
+     'power' => 1,
+     'text'  => 'This zombie does 1 damage to you when it enters your horde zone.'
+   }, 
+   {
+     'name'  => 'explosive',
+     'power' => 2,
+     'text'  => 'This zombie does 2 damage to you when it is destroyed. Remove it from the game.'
+   }, 
+   {
+     'name'  => 'thieving',
+     'power' => 2,
+     'text'  => 'Discard 1 card when this zombie enters your horde zone.'
+   }, 
+   {
+     'name'  => 'reanimating',
+     'power' => 1,
+     'text'  => 'Reanimate 1 zombie when this zombie enters your horde zone.'
+   }, 
+   {
+     'name'  => 'preventing',
+     'power' => 2,
+     'text'  => 'As long as this zombie is face up in your horde zone, you cannot active character abilities.'
+   }, 
+   {
+     'name'  => 'douchy',
+     'power' => 1,
+     'text'  => 'When this zombie enters your horde zone, draw another card from the zombie deck and place it face up in your horde zone.'
+   }, 
+   {
+     'name'  => 'pinata',
+     'power' => 2,
+     'text'  => 'Draw a card when this zombie is destroyed in your horde zone.'
+   }, 
+   {
+     'name'  => 'gelatinous',
+     'power' => 3,
+     'text'  => 'When this zombie enters your horde zone, take one item from your item zone and place it underneath this zombie. You cannot use the item as long as the item is underneath this zombie. When this zombie dies in your horde zone, take any items underneath it and placce them in your item zone.'
+   }, 
+   {
+     'name'  => 'resilient',
+     'power' => 1,
+     'text'  => 'Prevent the first 1 damage dealt to this zombie every turn.'
+   }, 
+   {
+     'name'  => 'normal',
+     'power' => 1,
+     'text'  => ''
+   }, 
+   {
+     'name'  => 'normal',
+     'power' => 2,
+     'text'  => ''
+   }, 
+   {
+     'name'  => 'normal',
+     'power' => 3,
+     'text'  => ''
+   }, 
+   {
+     'name'  => 'normal',
+     'power' => 4,
+     'text'  => ''
+   }
+  ]
+
+
